3cbf864f2f9a69a398e7150cd9f6a6fc90e6b769
changdaniel116 <changdaniel116@yahoo.com>
2019-05-23 02:28:16 -0500

    Started the category filter and populating questions with filters

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var jsondata = require('./data.json');
+var chicagocategorytree = require('./chicagocategorytree.json')
 
 
 
 var jsonreccomendations;
+
 var jsonfilter = {
     minrating: 1,
     maxnoratings: 100,
-    maxprice: 1,   
+    maxprice: 1,
+    night_owl: true,
+    categories = [],
+    gem: true
 }
 
 function filterMinRating(minrating, locations) {
@@ -89,6 +94,66 @@ function filterMinNoReviews(minnoreviews, locations) {
 
 }
 
+function filterNonMidnight(locations){
+
+    var overnight = false;
+    var filteredlocations = [];
+
+     locations.forEach(function(location){
+
+        if(location.hours == undefined){
+            
+            //Locations like parks have no hours, so valid overnight
+            overnight = true;
+        }
+        else{
+
+            location.hours[0].open.forEach(function(day){
+
+                if(day.is_overnight){overnight = true;}
+
+            });
+        }
+
+            if(overnight){filteredlocations.push(location);}
+            overnight = false;
+           
+     });
+
+     return filteredlocations;
+}
 
 
-console.log(filterMinRating(5, filterMaxNoReviews(30, jsondata)).length)
+
+function breakParent(parents){
+
+    var children = [];
+
+    parents.forEach(function (parent) {
+        
+        if(chicagocategorytree[parent] != null){
+            
+            for (const child of chicagocategorytree[parent].entries()) {
+
+                children.push(child)
+            }
+        }
+    })
+
+    return children;
+    
+}
+
+function addCategories(){
+    
+    
+    
+}
+
+function isDict(v) {
+    return typeof v==='object' && v!==null && !(v instanceof Array) && !(v instanceof Date);
+}
+
+console.log(filterNonMidnight(jsondata))
+//console.log(jsondata[0].hours[0].open)
+
